540807ba168bc57a88c901351e4d535d66540050
Dominik Pirngruber <dominik.pirngruber@reca.co.at>
2016-04-10 22:51:41 +0200

    mysql --> mysqli fix

--- android-im_WEBAPI/index.php
+++ android-im_WEBAPI/index.php
@@ -31,32 +31,27 @@ $dbUsername = "DB_USERNAME";
 $dbPassword = "DB_PASSWORD";
 $dbName = "DBNAME";
 
-
 $db = new MySQL($dbHost,$dbUsername,$dbPassword,$dbName);
 
 // if operation is failed by unknown reason
 define("FAILED", 0);
-
 define("SUCCESSFUL", 1);
 // when  signing up, if username is already taken, return this error
-define("SIGN_UP_USERNAME_CRASHED", 2);  
-// when add new friend request, if friend is not found, return this error 
+define("SIGN_UP_USERNAME_CRASHED", 2);
+// when add new friend request, if friend is not found, return this error
 define("ADD_NEW_USERNAME_NOT_FOUND", 2);
-
-// TIME_INTERVAL_FOR_USER_STATUS: if last authentication time of user is older 
+// TIME_INTERVAL_FOR_USER_STATUS: if last authentication time of user is older
 // than NOW - TIME_INTERVAL_FOR_USER_STATUS, then user is considered offline
 define("TIME_INTERVAL_FOR_USER_STATUS", 60);
-
 define("USER_APPROVED", 1);
 define("USER_UNAPPROVED", 0);
 
 
-$username = (isset($_REQUEST['username']) && count($_REQUEST['username']) > 0) 
-							? $_REQUEST['username'] 
+$username = (isset($_REQUEST['username']) && count($_REQUEST['username']) > 0)
+							? $_REQUEST['username']
 							: NULL;
 $password = isset($_REQUEST['password']) ? md5($_REQUEST['password']) : NULL;
 $port = isset($_REQUEST['port']) ? $_REQUEST['port'] : NULL;
-
 $action = isset($_REQUEST['action']) ? $_REQUEST['action'] : NULL;
 if ($action == "testWebAPI")
 {
@@ -68,53 +63,47 @@ if ($action == "testWebAPI")
 	exit;
 	}
 }
-
-if ($username == NULL || $password == NULL)	 
+if ($username == NULL || $password == NULL)
 {
 	echo FAILED;
 	exit;
 }
-
 $out = NULL;
 
-error_log($action."\r\n", 3, "error.log");
-
-$db->createDB();
-
-switch($action) 
+switch($action)
 {
-	
+
 	case "authenticateUser":
-		
-		
-		if ($userId = authenticateUser($db, $username, $password)) 
-		{					
-			
+
+
+		if ($userId = authenticateUser($db, $username, $password))
+		{
+
 			// providerId and requestId is Id of  a friend pair,
 			// providerId is the Id of making first friend request
 			// requestId is the Id of the friend approved the friend request made by providerId
-			
-			// fetching friends, 
-			// left join expression is a bit different, 
+
+			// fetching friends,
+			// left join expression is a bit different,
 			//		it is required to fetch the friend, not the users itself
-			
-			$sql = "select u.Id, u.username, (NOW()-u.authenticationTime) as authenticateTimeDifference, u.IP, 
-										f.providerId, f.requestId, f.status, u.port 
+
+			$sql = "select u.Id, u.username, (NOW()-u.authenticationTime) as authenticateTimeDifference, u.IP,
+										f.providerId, f.requestId, f.status, u.port
 							from friends f
-							left join users u on 
-										u.Id = if ( f.providerId = ".$userId.", f.requestId, f.providerId ) 
-							where (f.providerId = ".$userId." and f.status=".USER_APPROVED.")  or 
+							left join users u on
+										u.Id = if ( f.providerId = ".$userId.", f.requestId, f.providerId )
+							where (f.providerId = ".$userId." and f.status=".USER_APPROVED.")  or
 										 f.requestId = ".$userId." ";
-										 
+
 			//$sqlmessage = "SELECT * FROM `messages` WHERE `touid` = ".$userId." AND `read` = 0 LIMIT 0, 30 ";
-			
+
 			$sqlmessage = "SELECT m.id, m.fromuid, m.touid, m.sentdt, m.read, m.readdt, m.messagetext, u.username from messages m \n"
     . "left join users u on u.Id = m.fromuid WHERE `touid` = ".$userId." AND `read` = 0 LIMIT 0, 30 ";
-			
-	
-			if ($result = $db->query($sql))			
+
+
+			if ($result = $db->query($sql))
 			{
-					$out .= "<data>"; 
+					$out .= "<data>";
 					$out .= "<user userKey='".$userId."' />";
 					while ($row = $db->fetchObject($result))
 					{
@@ -126,15 +115,15 @@ switch($action)
 						else if (((int)$row->authenticateTimeDifference) < TIME_INTERVAL_FOR_USER_STATUS)
 						{
 							$status = "online";
-							 
+
 						}
 						$out .= "<friend  username = '".$row->username."'  status='".$status."' IP='".$row->IP."' userKey = '".$row->Id."'  port='".$row->port."'/>";
-												
+
 												// to increase security, we need to change userKey periodically and pay more attention
-												// receiving message and sending message 
-						
+												// receiving message and sending message
+
 					}
-						if ($resultmessage = $db->query($sqlmessage))			
+						if ($resultmessage = $db->query($sqlmessage))
 							{
 							while ($rowmessage = $db->fetchObject($resultmessage))
 								{
@@ -148,119 +137,117 @@ switch($action)
 			else
 			{
 				$out = FAILED;
-			}			
+			}
 		}
 		else
 		{
 				// exit application if not authenticated user
 				$out = FAILED;
 		}
-		
-	
-	
+
+
+
 	break;
-	
+
 	case "signUpUser":
 		if (isset($_REQUEST['email']))
 		{
-			 $email = $_REQUEST['email'];		
-			 	
-			 $sql = "select Id from  users 
+			 $email = $_REQUEST['email'];
+
+			 $sql = "select Id from  users
 			 				where username = '".$username."' limit 1";
-			 
-		
-			 				
+
+
+
 			 if ($result = $db->query($sql))
 			 {
-			 		if ($db->numRows($result) == 0) 
+			 		if ($db->numRows($result) == 0)
 			 		{
 			 				$sql = "insert into users(username, password, email)
-			 					values ('".$username."', '".$password."', '".$email."') ";		 					
-						 					
-			 					error_log("$sql", 3 , "error_log");
-							if ($db->query($sql))	
+			 					values ('".$username."', '".$password."', '".$email."') ";
+
+							if ($db->query($sql))
 							{
 							 		$out = SUCCESSFUL;
-							}				
+							}
 							else {
 									$out = FAILED;
-							}				 			
+							}
 			 		}
 			 		else
 			 		{
 			 			$out = SIGN_UP_USERNAME_CRASHED;
 			 		}
-			 }				 	 	
+			 }
 		}
 		else
 		{
 			$out = FAILED;
-		}	
+		}
 	break;
-	
+
 	case "sendMessage":
-	if ($userId = authenticateUser($db, $username, $password)) 
-		{	
+	if ($userId = authenticateUser($db, $username, $password))
+		{
 		if (isset($_REQUEST['to']))
 		{
-			 $tousername = $_REQUEST['to'];	
-			 $message = $_REQUEST['message'];	
-				
+			 $tousername = $_REQUEST['to'];
+			 $message = $_REQUEST['message'];
+
 			 $sqlto = "select Id from  users where username = '".$tousername."' limit 1";
-			 
-			 
-		
-					if ($resultto = $db->query($sqlto))			
+
+
+
+					if ($resultto = $db->query($sqlto))
 					{
 						while ($rowto = $db->fetchObject($resultto))
 						{
 							$uto = $rowto->Id;
 						}
-						$sql22 = "INSERT INTO `messages` (`fromuid`, `touid`, `sentdt`, `messagetext`) VALUES ('".$userId."', '".$uto."', '".DATE("Y-m-d H:i")."', '".$message."');";						
-						 					
-			 					error_log("$sql22", 3 , "error_log");
-							if ($db->query($sql22))	
+						$sql22 = "INSERT INTO `messages` (`fromuid`, `touid`, `sentdt`, `messagetext`) VALUES ('".$userId."', '".$uto."', '".DATE("Y-m-d H:i")."', '".$message."');";
+
+							if ($db->query($sql22))
 							{
 							 		$out = SUCCESSFUL;
-							}				
+							}
 							else {
 									$out = FAILED;
-							}				 		
+							}
 						$resultto = NULL;
-					}	
-			 				 	 	
+					}
+
 		$sqlto = NULL;
 		}
 		}
 		else
 		{
 			$out = FAILED;
-		}	
+		}
 	break;
-	
+
 	case "addNewFriend":
 		$userId = authenticateUser($db, $username, $password);
 		if ($userId != NULL)
 		{
-			
-			if (isset($_REQUEST['friendUserName']))			
-			{				
+
+			if (isset($_REQUEST['friendUserName']))
+			{
 				 $friendUserName = $_REQUEST['friendUserName'];
-				 
-				 $sql = "select Id from users 
-				 				 where username='".$friendUserName."' 
+
+				 $sql = "select Id from users
+				 				 where username='".$friendUserName."'
 				 				 limit 1";
 				 if ($result = $db->query($sql))
 				 {
 				 		if ($row = $db->fetchObject($result))
 				 		{
 				 			 $requestId = $row->Id;
-				 			 
+
 				 			 if ($row->Id != $userId)
 				 			 {
 				 			 		 $sql = "insert into friends(providerId, requestId, status)
 				 				  		 values(".$userId.", ".$requestId.", ".USER_UNAPPROVED.")";
-							 
+
 									 if ($db->query($sql))
 									 {
 									 		$out = SUCCESSFUL;
@@ -273,29 +260,29 @@ switch($action)
 							else
 							{
 								$out = FAILED;  // user add itself as a friend
-							} 		 				 				  		 
+							}
 				 		}
 				 		else
 				 		{
-				 			$out = FAILED;			 			
+				 			$out = FAILED;
 				 		}
-				 }				 				 
+				 }
 				 else
 				 {
 				 		$out = FAILED;
-				 }				
+				 }
 			}
 			else
 			{
 					$out = FAILED;
-			} 			
+			}
 		}
 		else
 		{
 			$out = FAILED;
-		}	
+		}
 	break;
-	
+
 	case "responseOfFriendReqs":
 		$userId = authenticateUser($db, $username, $password);
 		if ($userId != NULL)
@@ -315,20 +302,20 @@ switch($action)
 				  		{
 				  			$friendNamesQueryPart .= ",";
 				  		}
-				  		
+
 				  		$friendNamesQueryPart .= "'".$friendNames[$i]."'";
-				  		
-				  	}			  	
-				  	
+
+				  	}
+
 				  }
 				  if ($friendNamesQueryPart != NULL)
 				  {
 				  	$sqlApprove = "update friends set status = ".USER_APPROVED."
-				  					where requestId = ".$userId." and 
+				  					where requestId = ".$userId." and
 				  								providerId in (select Id from users where username in (".$friendNamesQueryPart."));
-				  				";		
+				  				";
 				  }
-				  				  
+
 			}
 			if (isset($_REQUEST['discardedFriends']))
 			{
@@ -343,21 +330,21 @@ switch($action)
 				  		{
 				  			$friendNamesQueryPart .= ",";
 				  		}
-				  		
+
 				  		$friendNamesQueryPart .= "'".$friendNames[$i]."'";
-				  		
-				  	}				  	
+
+				  	}
 				  }
 				  if ($friendNamesQueryPart != NULL)
 				  {
-				  	$sqlDiscard = "delete from friends 
-				  						where requestId = ".$userId." and 
+				  	$sqlDiscard = "delete from friends
+				  						where requestId = ".$userId." and
 				  									providerId in (select Id from users where username in (".$friendNamesQueryPart."));
 				  							";
-				  }						
+				  }
 			}
 			if (  ($sqlApprove != NULL ? $db->query($sqlApprove) : true) &&
-						($sqlDiscard != NULL ? $db->query($sqlDiscard) : true) 
+						($sqlDiscard != NULL ? $db->query($sqlDiscard) : true)
 			   )
 			{
 				$out = SUCCESSFUL;
@@ -365,51 +352,46 @@ switch($action)
 			else
 			{
 				$out = FAILED;
-			}		
+			}
 		}
 		else
 		{
 			$out = FAILED;
 		}
 	break;
-	
+
 	default:
-		$out = FAILED;		
-		break;	
+		$out = FAILED;
+		break;
 }
-
 echo $out;
-
-
-
 ///////////////////////////////////////////////////////////////
 function authenticateUser($db, $username, $password)
 {
-	
-	$sql22 = "select * from users 
-					where username = '".$username."' and password = '".$password."' 
+
+	$sql22 = "select * from users
+					where username = '".$username."' and password = '".$password."'
 					limit 1";
-	
+
 	$out = NULL;
 	if ($result22 = $db->query($sql22))
 	{
 		if ($row22 = $db->fetchObject($result22))
 		{
 				$out = $row22->Id;
-				
-				$sql22 = "update users set authenticationTime = NOW(), 
+
+				$sql22 = "update users set authenticationTime = NOW(),
 																 IP = '".$_SERVER["REMOTE_ADDR"]."' ,
-																 port = 15145 
+																 port = 15145
 								where Id = ".$row22->Id."
 								limit 1";
-				
-				$db->query($sql22);				
-								
-								
-		}		
+
+				$db->query($sql22);
+
+
+		}
 	}
-	
+
 	return $out;
 }
-
 ?>
